cb522eac15e9c0a350779e11a0d02315b697b4bc
dinzzularkadut <dinzzstore123@gmail.com>
2024-09-20 08:35:09 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    const balloons = document.querySelectorAll('.balloon');
-    balloons.forEach(balloon => {
-        balloon.style.animationDelay = Math.random() * 2 + 's';
-    });
-};
